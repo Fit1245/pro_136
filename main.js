77fdfc9631654c98c33c062431393642c1f5b58c
Fit1245 <87483283+Fit1245@users.noreply.github.com>
2022-07-02 20:57:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+video = "";
+Status = "";
+function setup() {
+    canvas = createCanvas(480,380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    image(video,0,0,480,380);
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "status : object detecting";
+}
+function modelLoaded() {
+    console.log("model Loaded!");
+    Status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
